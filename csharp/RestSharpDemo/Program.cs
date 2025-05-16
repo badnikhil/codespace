90f5b8d500ba5a898dfa8367034a88f1afce03f0
Nikhil <72620320+badnikhil@users.noreply.github.com>
2025-05-16 05:21:28 +0000

    added csharp

---
+++ csharp/RestSharpDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RestSharp;
+using System.Threading.Tasks;
+
+class Program
+{
+  static async Task Main(){
+    try{
+      const String _baseUrl = "https://api.apidash.dev";
+      var client = new RestClient(_baseUrl);
+
+      var request = new RestRequest("/country/data", Method.Get);
+
+      request.AddQueryParameter("code", "IND");
+      request.AddQueryParameter("code", "US");
+
+      var response = await client.ExecuteAsync(request);
+      Console.WriteLine("Status Code: " + (int)response.StatusCode);
+      Console.WriteLine("Response Content: " + response.Content);
+    }
+    catch(Exception ex){
+      Console.WriteLine("Error: " + ex);
+    }
+  }
+}
