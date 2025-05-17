3dacf28fc080c7915698be3d1417e024a791526e
Nikhil <72620320+badnikhil@users.noreply.github.com>
2025-05-17 18:13:38 +0000

    added java

--- csharp/RestSharpDemo/obj/Debug/net8.0/RestSharpDemo.AssemblyInfo.cs
+++ csharp/RestSharpDemo/obj/Debug/net8.0/RestSharpDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestSharpDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f5b8d500ba5a898dfa8367034a88f1afce03f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ca9cda59c6562b450d8a70c2cdf68cf2f09f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestSharpDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestSharpDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
